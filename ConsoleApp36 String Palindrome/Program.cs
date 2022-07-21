d2fa4460ad688aa9a46d2aa22daca11375401dce
Malay Modi <malay.modi@brainvire.com>
2022-07-21 11:16:48 +0530

    first commit

---
+++ ConsoleApp36 String Palindrome/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp36_String_Palindrome
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
